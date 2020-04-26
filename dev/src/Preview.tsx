6c7daa6aae62fed77a9419c07ec4baadc77f16f2
Marvin9 <marvinduff97@gmail.com>
2020-04-26 12:28:25 +0530

    Component preview setup

---
+++ dev/src/Preview.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Navigation } from '../../src';
+
+export const Preview: React.FC = () => (
+  <>
+    <h1>Navigation Preview</h1>
+    <Navigation />
+  </>
+);
