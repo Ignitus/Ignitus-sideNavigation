a80a788b8b2c98cdb70ab5003a0e77cbdd254480
Marvin9 <marvinduff97@gmail.com>
2020-04-26 12:04:02 +0530

    Development setup

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    "plugin:@typescript-eslint/recommended",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    "react/jsx-filename-extension": "off",
+    "import/extensions": "off",
+    "import/prefer-default-export": "off"
+  },
+};
