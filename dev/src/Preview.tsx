523b7068b42b973eedbee77e6ae7e69133fddedc
Marvin9 <marvinduff97@gmail.com>
2020-05-03 22:26:29 +0530

    Few Props can be modified in preview

--- dev/src/Preview.tsx
+++ dev/src/Preview.tsx
@@ -1,148 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Global } from '@emotion/core';
 
-import { SideNavigation, Container, GlobalStyle, LeftRow, navItem } from '../../src';
+import { ThemeWrapperProps } from '../../src/components/sideNavigation/component';
+import { defaultTheme } from '../../src/constants';
+import { SideNavigation, Container, GlobalStyle } from '../../src';
 
-const navItems: navItem[] = [
-  {
-    title: 'Atoms',
-    children: [
-      {
-        title: 'Color Palette',
-        route: '/colors',
-      },
-      {
-        title: 'Iconography',
-        route: '/icons',
-      },
-      {
-        title: 'Input Fields',
-        children: [
-          {
-            title: 'Search',
-            route: '/interface/searchInput',
-          },
-          {
-            title: 'Primary',
-            route: '/interface/primaryInput',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Molecules',
-    children: [
-      {
-        title: 'Overlay',
-        route: '/overlay',
-      },
-      {
-        title: 'Banners',
-        route: '/interface/banner',
-      },
-    ],
-  },
-  {
-    title: 'Atoms',
-    children: [
-      {
-        title: 'Color Palette',
-        route: '/colors',
-      },
-      {
-        title: 'Iconography',
-        route: '/icons',
-      },
-      {
-        title: 'Input Fields',
-        children: [
-          {
-            title: 'Search',
-            route: '/interface/searchInput',
-          },
-          {
-            title: 'Primary',
-            route: '/interface/primaryInput',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Molecules',
-    children: [
-      {
-        title: 'Overlay',
-        route: '/overlay',
-      },
-      {
-        title: 'Banners',
-        route: '/interface/banner',
-      },
-    ],
-  },
-  {
-    title: 'Atoms',
-    children: [
-      {
-        title: 'Color Palette',
-        route: '/colors',
-      },
-      {
-        title: 'Iconography',
-        route: '/icons',
-      },
-      {
-        title: 'Input Fields',
-        children: [
-          {
-            title: 'Search',
-            route: '/interface/searchInput',
-          },
-          {
-            title: 'Primary',
-            route: '/interface/primaryInput',
-          },
-          {
-            title: 'Molecules',
-            children: [
-              {
-                title: 'Overlay',
-                route: '/overlay',
-              },
-              {
-                title: 'Banners',
-                route: '/interface/banner',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Molecules',
-    children: [
-      {
-        title: 'Overlay',
-        route: '/overlay',
-      },
-      {
-        title: 'Banners',
-        route: '/interface/banner',
-      },
-    ],
-  },
-];
+import { Docs } from './docs';
+import { StyledContent } from './styles';
+import { navItems } from './constants';
 
-export const Preview: React.FC = () => (
-  <Router>
-    <Global styles={GlobalStyle} />
-    <Container>
-      <SideNavigation navItems={navItems} />
-      <LeftRow>Content</LeftRow>
-    </Container>
-  </Router>
-);
+export const Preview: React.FC = () => {
+  const [theme, updateTheme] = useState<ThemeWrapperProps>(defaultTheme);
+
+  return (
+    <Router>
+      <Global styles={GlobalStyle} />
+      <Container>
+        <SideNavigation navItems={navItems} navBackground={theme.navBackground} theme={theme.theme} />
+        <StyledContent>
+          <Docs updateTheme={updateTheme} theme={theme} />
+        </StyledContent>
+      </Container>
+    </Router>
+  );
+};
---
+++ dev/src/docs/index.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+
+import { ThemeWrapperProps } from '../../../src/components/sideNavigation/component';
+import { NavigationHeading as Heading } from '../../../src/components/sideNavigation/styles';
+
+import { Emoji } from './components/Emoji';
+import { Input, ListItem, MediumHeading } from './styles';
+
+interface DocsProps {
+  theme: ThemeWrapperProps;
+  updateTheme: React.Dispatch<React.SetStateAction<ThemeWrapperProps>>;
+}
+
+export const Docs: React.FC<DocsProps> = ({ theme, updateTheme }) => (
+  <React.Fragment>
+    <Heading style={{ cursor: 'pointer' }}>
+      <u>API</u>
+      <Emoji emoji="🚀" label="teacher" />
+    </Heading>
+    <MediumHeading>{'<SideNavigation />'}</MediumHeading>
+
+    <ListItem>
+      Background :
+      <Input
+        type="text"
+        placeholder="#000"
+        value={theme.navBackground}
+        onChange={e => {
+          updateTheme({
+            ...theme,
+            navBackground: e.target.value,
+          });
+        }}
+      />
+    </ListItem>
+
+    <ListItem>
+      Text color :
+      <Input
+        type="text"
+        placeholder="#000"
+        value={theme.theme.textColor}
+        onChange={e => {
+          updateTheme({
+            ...theme,
+            theme: {
+              ...theme.theme,
+              textColor: e.target.value,
+            },
+          });
+        }}
+      />
+    </ListItem>
+
+    <ListItem>
+      Hover background :
+      <Input
+        type="text"
+        placeholder="#000"
+        value={theme.theme.hover.navItemBackground}
+        onChange={e => {
+          updateTheme({
+            ...theme,
+            theme: {
+              ...theme.theme,
+              hover: {
+                ...theme.theme.hover,
+                navItemBackground: e.target.value,
+              },
+            },
+          });
+        }}
+      />
+    </ListItem>
+
+    <ListItem>
+      Hover text color :
+      <Input
+        type="text"
+        placeholder="#000"
+        value={theme.theme.hover.navItemTextColor}
+        onChange={e => {
+          updateTheme({
+            ...theme,
+            theme: {
+              ...theme.theme,
+              hover: {
+                ...theme.theme.hover,
+                navItemTextColor: e.target.value,
+              },
+            },
+          });
+        }}
+      />
+    </ListItem>
+  </React.Fragment>
+);
